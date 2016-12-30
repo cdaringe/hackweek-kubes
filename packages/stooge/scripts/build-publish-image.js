0319c348a8d830304b2e544f36f37a9b69c57593
cdaringe <christopher.dieringer@gmail.com>
2016-12-30 15:04:19 -0700

    :bug: fix string interpolation

--- packages/stooge/scripts/build-publish-image.js
+++ packages/stooge/scripts/build-publish-image.js
@@ -4,7 +4,7 @@ const cp = require('child_process')
 const pkg = require('../package.json')
 
 const tag = `cdaringe/stooge:${pkg.version}`
-console.log('building & publishing: ${tag} to the docker registry')
+console.log(`building & publishing: ${tag} to the docker registry`)
 cp.exec([
   `rm -rf node_modules/common`,
   `npm install`,
