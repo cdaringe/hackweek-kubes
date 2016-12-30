8111e61409d17ce5c77fe1f03e028ba6f28867bf
cdaringe <christopher.dieringer@gmail.com>
2016-12-30 15:00:40 -0700

    :art: improve build deploy of stooge, use fatter image for dev

---
+++ packages/stooge/scripts/build-publish-image.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const cp = require('child_process')
+const pkg = require('../package.json')
+
+const tag = `cdaringe/stooge:${pkg.version}`
+console.log('building & publishing: ${tag} to the docker registry')
+cp.exec([
+  `rm -rf node_modules/common`,
+  `npm install`,
+  `npm ls`,
+  `docker build . -t ${tag}`,
+  `docker push ${tag}`
+].join(' && '))
